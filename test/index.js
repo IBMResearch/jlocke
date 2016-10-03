ddef56299c4137449fe3ef044387234d1f821512
Jesús Pérez <jesusprubio@gmail.com>
2016-10-03 15:36:45 +0200

    Some fixes in the tests.
    We don't need to drop the collection in Travis.

--- test/index.js
+++ test/index.js
@@ -17,8 +17,8 @@ const port = 8888;
 const url = 'mongodb://localhost:27017/requests-monitor';
 const app = express();
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 console.log(`Starting, connecting to the DB: ${url}`); // eslint-disable-line no-console
 MongoClient.connect(url)
@@ -35,36 +35,34 @@ MongoClient.connect(url)
   // eslint-disable-next-line no-console
   const server = app.listen(port, () => { console.log(`Example app listening on port: ${port}`); });
 
-  db.collection('requests').drop()
-  .then(() =>
-    test('with a valid server', (assert) => {
-      assert.plan(10);
+  test('with a valid server', (assert) => {
+    assert.plan(10);
 
-      makeReq(`http://127.0.0.1:${port}`)
-      .then((httpRes) => {
-        assert.equal(httpRes[1], 'Hello World!');
+    makeReq(`http://127.0.0.1:${port}`)
+    .then((httpRes) => {
+      assert.equal(httpRes[1], 'Hello World!');
 
-        db.collection('requests').find().toArray()
-        .then((res) => {
-          assert.equal(res.length, 1);
-          assert.deepEqual(Object.keys(res[0]), [
-            '_id', 'path', 'method', 'protocol', 'ip',
-            'headers', 'originalUrl',
-          ]);
-          assert.equal(res[0].path, '/');
-          assert.equal(res[0].method, 'GET');
-          assert.equal(res[0].protocol, 'http');
-          assert.equal(res[0].ip, '::ffff:127.0.0.1');
-          assert.equal(res[0].headers.host, '127.0.0.1:8888');
-          assert.equal(res[0].headers.connection, 'close');
-          assert.equal(res[0].originalUrl, '/');
+      db.collection('requests').find().toArray()
+      .then((res) => {
+        assert.equal(res.length, 1);
+        assert.deepEqual(Object.keys(res[0]), [
+          '_id', 'path', 'method', 'protocol', 'ip',
+          'headers', 'originalUrl',
+        ]);
+        assert.equal(res[0].path, '/');
+        assert.equal(res[0].method, 'GET');
+        assert.equal(res[0].protocol, 'http');
+        assert.equal(res[0].ip, '::ffff:127.0.0.1');
+        assert.equal(res[0].headers.host, '127.0.0.1:8888');
+        assert.equal(res[0].headers.connection, 'close');
+        assert.equal(res[0].originalUrl, '/');
 
-          // We need to close to allow the test keep passing.
-          db.close();
-          server.close();
-        });
-      });
-    })
-  );
-});// eslint-disable-next-line no-console
-// .catch((err) => { throw.error(`Connecting to the DB: ${err.message}`); });
+        // We need to close to allow the test keep passing.
+        db.close();
+        server.close();
+      })
+      .catch((err) => { assert.fail(`Getting the requests: ${err.message}`); });
+    });
+  });
+})
+.catch((err) => { throw Error(`Connecting to the DB: ${err.message}`); });
