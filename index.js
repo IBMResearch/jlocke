dcfea8217b4f2241ef26fe7b2a777c746cecf110
Jesús Pérez <jesusprubio@gmail.com>
2016-10-04 11:48:40 +0200

    Performance improved thanks to bluebird.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Promise = require('bluebird');
 const dbg = require('debug')('express-middleware-todb');
 
 const getLocation = require('tiny-promisify')(require('iplocation'));
@@ -31,6 +32,14 @@ module.exports = (db, opts = {}) =>
     // Getting the collection to store the requests info if the proper options is passed.
     const outCol = opts.col || 'requests';
 
+    // Adding geolocation info if the proper options is passed.
+    // We only need to check for it if the user pass the option. (default: false).
+    let getId = () => Promise.resolve();
+    if (opts.idFunc) {
+      getId = opts.idFunc;
+      dbg('The user passed a function to get the user ID ...');
+    }
+
     // We only need to check for it if the user pass the option. (default: false).
     let getLoc = () => Promise.resolve();
     // Adding geolocation info if the proper options is passed.
@@ -39,20 +48,16 @@ module.exports = (db, opts = {}) =>
       dbg('The user asked for the location ...');
     }
 
-    // Adding geolocation info if the proper options is passed.
-    if (opts.idFunc) {
-      meta.userId = opts.idFunc(req);
-      dbg('The user passed a function to get the user ID ...');
-    }
-
-    getLoc(req.ip)
-    .then((geoInfo) => {
-      if (geoInfo) { meta.geo = geoInfo; }
-
+    Promise.join(getId(req, opts.app), getLoc(req.ip))
+    .then((result) => {
+      if (result[0]) { meta.userId = result[0]; }
+      if (result[1]) { meta.geo = result[1]; }
       dbg('Inserting found request metadata in the DB', meta);
 
       // TODO: Make this connection an op
       db.collection(outCol).insertOne(meta)
-      .then(() => dbg('New metadata correctly inserted'));
-    });
+      .then(() => dbg('New metadata correctly inserted'))
+      .catch((err) => { throw Error(`Inserting the metadata: ${err.message}`); });
+    })
+    .catch((err) => { throw Error(`Getting the user ID or IP geolocation: ${err.message}`); });
   };
